72425f609dec8b8bf3faf1e693bfaf3b86be3429
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-03-14 08:13:09 -0700

    updates

--- exec sp_Get_EQuIS_Results_w_ALs_test.sql
+++ exec sp_Get_EQuIS_Results_w_ALs_test.sql
@@ -7,24 +7,23 @@ exec  [hai].[sp_Get_EQuIS_Results_w_ALs_test]
 	
 		 47, --@facility_id int,
 		 null, --@location_groups varchar (2000),
-		 'EH-HA16-GT-22' , --@locations varchar (2000),
+		 null, --@locations varchar (2000),
 		 'n', --@sample_type varchar(200),
-		 'EH_SE_2016Oct', --'PGE-P39-EB', --'0459007-PAMS|0459007-PAMS-Back',--'ROW_GW_2016_1Q|ROW_GW_2016_2q|ROW_GW_2016_3Q', --@task_codes varchar (1000),
+		 '0459007-pams', --'0459007-PAMS|0459007-PAMS-Back',--'ROW_GW_2016_1Q|ROW_GW_2016_2q|ROW_GW_2016_3Q', --@task_codes varchar (1000),
 		 null, --@SDG varchar (2000),
 		 '1/1/2000', --@start_date datetime, --= 'jan 01 1900 12:00 AM',
 		'1/1/2050', --datetime,  -- ='dec 31 2050 11:59 PM',
-		 null, --@analyte_groups varchar(2000),
+		 'pge air btex + napth|pge air pahs', --@analyte_groups varchar(2000),
 		 null, --@cas_rns varchar (2000),
 		 null,-- --@matrix_codes varchar (500),
 		 null, --'ug/m3',  --@target_unit varchar(100),
 		 'rl', -- @limit_type varchar (10) = 'RL',
-		 null, --@action_level_codes varchar (500),
+		 'pge-sl-aa-201605', --@action_level_codes varchar (500),
 		 null, --'PM10_Max_1h_Avg|PM10_Overall_Avg|TVOC_15_Min_Avg|TVOC_Overall_Avg', -- @loc_param_codes varchar(2000) ,
-		 'Elevation Range|Mudline Elevation', --@include_loc_params varchar(10) = 'N',
+		 null, --'Elevation Range|Mudline Elevation', --@sample parameters,
 		 '< # Q', --@user_qual_def varchar (10),
 		 'y', --@show_val_yn varchar(10) ,
 		 null, --@coord_type varchar (20),
 		 'n' -- @detects_only varchar (10) = 'N'  /*returns all samples/chemicals if any one sample had that chemcial detected*/
 	
 
-	
